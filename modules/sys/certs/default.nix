0ec573ad63a890e9e6c69efcaebdd260c6829ee6
Albert O'Shea <albertoshea2@gmail.com>
2025-10-17 22:41:38 +1100

    add ca certs aspect

---
+++ modules/sys/certs/default.nix
@@ -0,0 +1,57 @@
+{ lib, ... }:
+let
+  mkEnvVars =
+    path:
+    lib.mapAttrs (_: value: lib.mkDefault value) {
+      CURL_CA_BUNDLE = path;
+      GIT_SSL_CAINFO = path;
+      NIX_SSL_CERT_FILE = path;
+      NODE_EXTRA_CA_CERTS = path;
+      REQUESTS_CA_BUNDLE = path;
+      SSL_CERT_FILE = path;
+    };
+
+  nixosModule =
+    { pkgs, lib, ... }:
+    let
+      bundlePath = "/etc/ssl/certs/ca-bundle.crt";
+      envVars = mkEnvVars bundlePath;
+    in
+    {
+      config = {
+        environment.systemPackages = lib.mkAfter [ pkgs.cacert ];
+        environment.variables = envVars;
+        environment.sessionVariables = envVars;
+
+        nix.settings.ssl-cert-file = lib.mkDefault bundlePath;
+      };
+    };
+
+  hmModule =
+    { pkgs, lib, ... }:
+    let
+      bundlePath = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      envVars = mkEnvVars bundlePath;
+    in
+    {
+      config = {
+        home.packages = lib.mkAfter [ pkgs.cacert ];
+        home.sessionVariables = envVars;
+
+        programs.git.extraConfig.http.sslCAInfo = lib.mkDefault bundlePath;
+      };
+    };
+in
+{
+  autix.aspects.certificates = {
+    description = "Ensure CA certificates are installed and exported consistently.";
+    home = {
+      targets = [ "*" ];
+      modules = [ hmModule ];
+    };
+    nixos = {
+      targets = [ "*" ];
+      modules = [ nixosModule ];
+    };
+  };
+}
